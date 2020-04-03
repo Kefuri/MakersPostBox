a779323ca22fa73ab31b481264ba46ba3ec6f895
Student <emilywright212@gmail.com>
2020-04-03 15:53:48 +0100

    Refactor sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,2 @@
 module SessionsHelper
-  def successful_session(user)
-    session[:current_user_id] = user.id
-    flash[:notice] = "Log in successful!"
-    redirect_to '/home/index'
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:current_user_id]) if session[:current_user_id]
-  end
 end

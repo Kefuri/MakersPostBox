082c8b42483171cd6a9acb67e365b18990173904
Student <student@makerss-air.home>
2020-04-03 12:15:35 +0100

    add element in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ premade_user =  User.create(first_name: "Premade", last_name: "Packager", email:
 
 premade_package_dogs = Package.create(users_id: premade_user.id, created_at: Time.now, updated_at: Time.now)
 
+
 elements_for_dog_package = [
   ["https://bit.ly/2Jxd8tE", "dog with glasses"],
   ["https://media.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.gif", "Husky Ears Gif"],
@@ -19,3 +20,7 @@ elements_for_dog_package = [
   ["https://images.unsplash.com/photo-1548658166-136d9f6a7e76?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60", "puppy yawning"],
   ["https://images.unsplash.com/photo-1516371535707-512a1e83bb9a?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", "dog"], 
 ]
+
+elements_for_dog_package.each do | link, alt_tag |
+  Element.create(link: link, alt_tag: alt_tag, packages_id: premade_package_dogs.id)
+end

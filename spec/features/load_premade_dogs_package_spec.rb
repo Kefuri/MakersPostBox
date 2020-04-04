d934963fd4f2cba858d0a5cf8d84296eb939a4c8
Student <emilywright212@gmail.com>
2020-04-04 12:20:56 +0100

    Fix Dogs test to account for user needs to be signed in

--- spec/features/load_premade_dogs_package_spec.rb
+++ spec/features/load_premade_dogs_package_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Dogs", type: :feature do
   scenario "User loads the page and sees dog pictures" do
+    sign_up
     visit('/premade/dogs')
     find("img[src='https://bit.ly/2Jxd8tE']")
   end
